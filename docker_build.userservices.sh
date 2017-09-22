2c33803a773ccd245715ec4c3355c9bf8644915e
Clint Beacock <clint.beacock@gmail.com>
2017-09-22 12:17:15 -0400

    Updated Dockerfiles to set up live compiling so we don't need to rebuild images for each change

---
+++ docker_build.userservices.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -eu
+
+# Update config to work with Docker hostnames
+sed -i -e 's/localhost/mongo/' _config/userservices/message_store.local.json
+sed -i -e 's/localhost/mongo/' _config/userservices/notification_store.local.json
+sed -i -e 's/localhost/mongo/' _config/userservices/permission_store.local.json
+sed -i -e 's/localhost/mongo/' _config/userservices/profile_store.local.json
+sed -i -e 's/localhost/mongo/' _config/userservices/session_store.local.json
+sed -i -e 's/localhost/mongo/' _config/userservices/user_store.local.json
+sed -i -e 's/localhost/styx/' _config/userservices/metricservices_client.local.json
+sed -i -e 's/localhost/styx/' _config/userservices/dataservices_client.local.json
+sed -i -e 's/localhost/styx/' _config/userservices/userservices_client.local.json
+
+make build
